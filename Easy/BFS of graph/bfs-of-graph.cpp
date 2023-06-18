ba0cdaf7e4680fdcf3a8ec0aa6fae3a4d368a886
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-19 00:14:13 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -6,18 +6,17 @@ using namespace std;
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    void bfs(vector<int> adj[],unordered_map<int,bool>&mappe,vector<int >&ans){
+    void bfs(vector<int>&ans,unordered_map<int,bool>&mappe,int i,vector<int> adj[]){
         queue<int>q;
-        mappe[0]=true;
         q.push(0);
-        ans.push_back(0);
+        mappe[i]=true;
         while(!q.empty()){
             int node=q.front();
             q.pop();
+            ans.push_back(node);
             for(auto x:adj[node]){
-                if(!mappe[x]){
+                if(mappe[x]==false){
                     q.push(x);
-                    ans.push_back(x);
                     mappe[x]=true;
                 }
             }
@@ -29,39 +28,12 @@ class Solution {
         unordered_map<int,bool>mappe;
         // for(int i=0;i<V;i++){
             // if(!mappe[i]){
-                bfs(adj,mappe,ans);
-            // }
+                bfs(ans,mappe,0,adj);
+        //     }
         // }
         return ans;
     }
 };
-     // Function to return Breadth First Traversal of given graph.
-//     void bsf(int V,vector<int>adj[],unordered_map<int,bool>&mappe,vector<int>&ans){
-//         queue<int >q;
-//         mappe[0]=true;
-//         q.push(0);
-//         ans.push_back(0);
-//         while(!q.empty()){
-//             int a=q.front();
-//             q.pop();
-            
-//             for(auto it:adj[a]){
-//                 if(!mappe[it]){
-//                     mappe[it]=true;
-//                     q.push(it);
-//                     ans.push_back(it);
-//                 }
-//             }
-//         }
-//     }
-//     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-//         vector<int>ans;
-//         unordered_map<int, bool>mappe;
-//         // Code here
-//         bsf(V,adj,mappe,ans);
-//         return ans;
-//     }
-// };
 
 //{ Driver Code Starts.
 int main() {
