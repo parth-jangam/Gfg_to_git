ab3eedd2d97f69b773d54f7a0d17a601dac436cb
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-20 00:29:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -6,86 +6,46 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool cycle(vector<int> adj[],unordered_map<int,bool>&mappe,int i){
-        mappe[i]=true;
-        queue<pair<int ,int>>q;
-        q.push({i,-1});
-        
+    bool bfs(int i, vector<int> adj[]){
+        queue<int>q;
+        unordered_map<int,bool>vis;
+        unordered_map<int,int>parent;
+        q.push(i);
+        vis[i]=true;
+        parent[i]=-1;
         while(!q.empty()){
-            int node=q.front().first;
-            int parent=q.front().second;
+            int node=q.front();
             q.pop();
+            int par=parent[node];
             for(auto x:adj[node]){
-                if(!mappe[x]){
-                    q.push({x,node});
-                        mappe[x]=1;
+                if(vis[x] && par!=x){
+                    return true;
                 }
                 else{
-                    if(mappe[x]==1  && x!=parent){
-                        return true;
+                    if(!vis[x] && par!=x){
+                        q.push(x);
+                        vis[x]=true;
+                        parent[x]=node;
                     }
                 }
-                
             }
+            
         }
         return false;
+        
     }
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        // bool check=false;
-        unordered_map<int,bool>mappe;
+        
+        
         for(int i=0;i<V;i++){
-            if(mappe[i]!=1){
-                if(cycle(adj, mappe,i)){
-                    return true;
-                }
-                
+            if(bfs(i,adj)){
+                return true;
             }
         }
         return false;
-        
     }
 };
-// class Solution {
-//   public:
-//     // Function to detect cycle in an undirected graph.
-//     bool bfs(int i,vector<int>adj[],unordered_map<int,int>&mappe){
-//         queue<pair<int,int>>q;
-//         q.push({i,-1});
-//         mappe[i]=1;
-//         while(!q.empty()){
-//             int node=q.front().first;
-//             int parent=q.front().second;
-//             q.pop();
-//             for(auto x:adj[node]){
-//                 if(!mappe[x]){
-//                     q.push({x,node});
-//                     mappe[x]=1;
-//                 }
-//                 else{
-//                     if(mappe[x]==1 && x!=parent){
-//                         return true;
-//                     }
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-//     bool isCycle(int V, vector<int> adj[]) {
-//         // Code here
-        
-//         unordered_map<int,int>mappe;
-//         // for(int i=0;i<V;i++)vis[V]=-1;
-//         for(int i=0;i<V;i++){
-//             if(mappe[i]!=1){
-//                 if(bfs(i,adj,mappe)){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
 
 //{ Driver Code Starts.
 int main() {
