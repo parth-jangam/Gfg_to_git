4a58039b0517897bbc6216074c747dee6106f461
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-07 11:40:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nearly sorted/nearly-sorted.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        vector<int>ans(num);
+        int i=0;
+        
+        int j=0;
+        priority_queue<int, vector<int>, greater<int>>q;
+        while(i<num){
+            q.push(arr[i]);
+            if(q.size()==K+1){
+                ans[j]=q.top();
+                q.pop();
+                j++;
+            }
+            i++;
+        }
+        while(!q.empty()){
+            ans[j]=q.top();
+            q.pop();
+            j++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
