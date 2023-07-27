0b5b2a2bee4f4024c918ab4610c59a89cbf7c34b
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-27 16:10:47 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -6,29 +6,32 @@ using namespace std;
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    void bfs(vector<int>&ans,unordered_map<int,bool>&mappe,int i,vector<int> adj[]){
+    void bfs(vector<int> adj[],int i,vector<int>&ans,int vis[]){
         queue<int>q;
-        q.push(0);
-        mappe[i]=true;
+        vis[i]=1;
+        // ans.push_back(i);
+        q.push(i);
         while(!q.empty()){
             int node=q.front();
-            q.pop();
             ans.push_back(node);
+            q.pop();
             for(auto x:adj[node]){
-                if(mappe[x]==false){
+                if(vis[x]==0){
                     q.push(x);
-                    mappe[x]=true;
+                    vis[x]=1;
                 }
             }
+            
         }
     }
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         // Code here
+        int vis[V]={0};
         vector<int>ans;
-        unordered_map<int,bool>mappe;
+        bfs(adj,0,ans,vis);
         // for(int i=0;i<V;i++){
-            // if(!mappe[i]){
-                bfs(ans,mappe,0,adj);
+        //     if(!vis[i]){
+                
         //     }
         // }
         return ans;
