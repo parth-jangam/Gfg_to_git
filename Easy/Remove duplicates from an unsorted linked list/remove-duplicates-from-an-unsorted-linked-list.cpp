4b19579e1e6286bf9763aa766b25ef9d8cabe561
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-12-12 23:27:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    void removee(Node* &temp,Node* temp1,unordered_map<int,int>&mappe){
+        if(temp->next==NULL){
+            return;
+        }
+        removee(temp->next,temp1,mappe);
+        if(mappe[temp->next->data]>1){
+            mappe[temp->next->data]--;
+            temp1=temp->next;
+            temp->next=temp1->next;
+            temp1->next=NULL;
+        }
+    }
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+     unordered_map<int,int>mappe;
+     Node* temp=head;
+     while(temp!=NULL){
+         mappe[temp->data]++;
+         temp=temp->next;
+     }
+     temp=head;
+     Node* temp1=nullptr;
+    //  while(temp!=NULL && temp->next!=NULL){
+    //      if(mappe[temp->next->data]>1){
+    //          mappe[temp->next->data]--;
+    //          temp1=temp->next;
+    //          temp->next=temp1->next;
+    //          temp1->next=NULL;
+    //      }
+    //      else{
+    //          temp=temp->next;
+    //      }
+    //  }
+    removee(temp,temp1,mappe);
+     
+      return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
